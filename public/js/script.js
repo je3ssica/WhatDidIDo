078259ecf2a602ecb5c888d0c09b89f1f0dfb250
Jessica Strömberg <jessica.stromberg@live.se>
2017-04-18 10:34:49 +0200

    design to dashboard accordions

--- public/js/script.js
+++ public/js/script.js
@@ -2,10 +2,12 @@ $(document).ready(function(){
   $('.modal').modal();
   $('#signUpModal').modal('open');
   $('#loginModal').modal('open');
-  
+
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
+    selectYears: 15, // Creates a dropdown of 15 years to control year
+    format: 'dd-mm-yyyy',
+    firstDay: 'Monday',
   });
 
   $('input.autocomplete').autocomplete({
