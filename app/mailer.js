8f06153d1613d9de5e788eef0c8b7e5ad4a3ed38
Jessica Strömberg <jessica.stromberg@live.se>
2017-04-18 13:02:47 +0200

    nodemailer setup

---
+++ app/mailer.js
@@ -0,0 +1,28 @@
+'use strict';
+const nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'je3ssica@gmail.com',
+        pass: 'testingtesting123'
+    }
+});
+
+// setup email data with unicode symbols
+let mailOptions = {
+    from: '"WhatDidIDo" <je3ssica@gmail.com>', // sender address
+    to: 'jessica.stromberg@live.se', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world ?', // plain text body
+    html: '<b>Hello world ?</b>' // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+});
